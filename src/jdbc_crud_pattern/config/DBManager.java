51da2db5f20b93f6ace4d72ada86c1ee0fe86e33
Jorge Beckhauser <jorgevibe@gmail.com>
2022-11-28 16:12:51 -0300

    classe DBManager

---
+++ src/jdbc_crud_pattern/config/DBManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jdbc_crud_pattern.config;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Jorge
+ */
+public class DBManager {
+
+    static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("jdbc.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost/fiap";
+            String username = "root";
+            String password = "root";
+            connection = DriverManager.getConnection(url, username, password);
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+
+        }
+        return connection;
+    }
+}
